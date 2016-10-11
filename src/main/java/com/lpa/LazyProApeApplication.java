e90255354ef62f252d246e7c92a1fb426d0783dc
王存山 <wangcunshan62@camelotchina.com>
2016-10-11 15:14:17 +0800

    创建工程

---
+++ src/main/java/com/lpa/LazyProApeApplication.java
@@ -0,0 +1,12 @@
+package com.lpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyProApeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyProApeApplication.class, args);
+	}
+}
